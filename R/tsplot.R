5a94c5f62fca5dfacce06d383c1f51eb3cdc7a2b
Nicky Poison <nickpoison@hotmail.com>
2017-05-23 18:21:49 -0400

    1.7.7

--- R/tsplot.R
+++ R/tsplot.R
@@ -2,9 +2,9 @@ tsplot <- function(series, main=NULL, ylab=deparse(substitute(series)), ... ){
   x = as.ts(series)
   topper = ifelse(is.null(main), 0, 1)  
   par(mar=c(2.5,2.5,1+topper,.5), mgp=c(1.6,.6,0), cex.main=1.2)
-  plot(x, type='n', main=main, ylab=ylab ) 
+  plot(x, type='n', main=main, ylab=ylab, ... ) 
   grid(lty=1, col=gray(.9))
-  lines(x, ...)
+  lines(x, ... )
   }
   
   
