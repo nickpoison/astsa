3c014261b99df6b2da126b146d5822b1b724c166
Nicky Poison <nickpoison@hotmail.com>
2017-05-24 14:00:07 -0400

    v1.7.7

--- R/tsplot.R
+++ R/tsplot.R
@@ -1,11 +1,11 @@
-tsplot <- function(series, main=NULL, ylab=deparse(substitute(series)), type='l', ... ){
+tsplot <- function(series, main=NULL, ylab=deparse(substitute(series)), type='l', margins=0, ... ){
   par = graphics::par
   plot = graphics::plot
   grid = graphics::grid
   lines = graphics::lines
   x = stats::as.ts(series)
   topper = ifelse(is.null(main), 0, 1)  
-  par(mar=c(2.5,2.5,1+topper,.5), mgp=c(1.6,.6,0), cex.main=1.2)
+  par(mar=c(2.5,2.5,1+topper,.5)+margins, mgp=c(1.6,.6,0), cex.main=1.2)
   plot(x, type='n', main=main, ylab=ylab, ... ) 
   grid(lty=1, col=gray(.9))
   lines(x, type=type, ... )
