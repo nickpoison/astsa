c9c0f1be6cfd0b2bc03afcc2abef910c58b54cd7
Nicky Poison <nickpoison@hotmail.com>
2017-05-26 20:45:55 -0400

    v1.7.7

--- R/upastsa.R
+++
@@ -1,6 +0,0 @@
-upastsa <- function(check=TRUE){
-# check if devtools is installed
-if(check) {if (!'devtools' %in% installed.packages()[,1]) install.packages(devtools) } 
-# update astsa
-devtools::install_github("nickpoison/astsa")
-}
