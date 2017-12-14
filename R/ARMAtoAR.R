1e6b800321d1057ab4e77a4559c442437b6bcb10
Nicky Poison <nickpoison@hotmail.com>
2017-12-13 21:45:35 -0500

    v1.8

--- R/ARMAtoAR.R
+++ R/ARMAtoAR.R
@@ -1,4 +1,5 @@
-ARMAtoAR <- function(ar = numeric(), ma = numeric(), lag.max){
+ARMAtoAR <- function(ar = 0, ma = 0, lag.max=20){
+             ARMAtoMA <- stats::ARMAtoMA
              arinv <- -ma
              mainv <- -ar
              return(round(ARMAtoMA(arinv, mainv, lag.max), 3))
