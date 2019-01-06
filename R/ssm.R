947ac96e35895175127d04b31ffa44b04a19ec9b
Nicky Poison <nickpoison@hotmail.com>
2019-01-06 15:26:30 -0500

    ssm.R

---
+++ R/ssm.R
@@ -0,0 +1,42 @@
+ssm <-
+function(y, A, phi, alpha, sigw, sigv, fixphi=FALSE){
+strt = tsp(as.ts(y))[1]
+frq  = tsp(as.ts(y))[3]
+x00 = mean(y[1:5])
+P00 = var(y) 
+num = length(y)
+B = A
+A = array(A, dim=c(1,1,num))
+#
+# function to calc likelihood
+Linn=function(para){
+  if (fixphi){ phi=phi; alpha = para[1]; sigw = para[2];  sigv = para[3] 
+  } else {phi=para[1]; alpha = para[2];   sigw = para[3];  sigv = para[4]}
+ kf = Kfilter1(num,y,A,x00,P00,phi,alpha,0,sigw,sigv,rep(1,num))
+ return(kf$like) 
+}
+#
+# Estimation
+if (fixphi){ init.par = c(alpha,sigw,sigv) 
+ } else {init.par = c(phi,alpha,sigw,sigv)
+ }
+est = optim(init.par, Linn, NULL, method="BFGS", hessian=TRUE, control=list(trace=1,REPORT=1)) 
+SE = sqrt(diag(solve(est$hessian))) 
+#
+if (fixphi){ phat = c(phi, est$par)
+} else {phat = est$par}
+# run filter /smoother with estimates
+ks = Ksmooth1(num,y,A,x00,P00,phat[1],phat[2],0,phat[3],phat[4],rep(1,num))
+Xp = ts(as.vector(ks$xp), start=strt, freq=frq) + phat[2]
+Pp = ts(as.vector(ks$Pp), start=strt, freq=frq)
+Xs = ts(as.vector(ks$xs), start=strt, freq=frq) + phat[2]
+Ps = ts(as.vector(ks$Ps), start=strt, freq=frq)
+estimate = est$par 
+u = cbind(estimate, SE) 
+if (fixphi){rownames(u)=c("alpha", "sigw", "sigv"); est$par[2:3] = abs(est$par[2:3]) 
+ } else { rownames(u)=c("phi", "alpha", "sigw", "sigv"); est$par[3:4] = abs(est$par[3:4])
+ }
+print(u)
+timserout = list(Xp=Xp, Pp=Pp, Xs=Xs, Ps=Ps)
+return(invisible(timserout))
+}
