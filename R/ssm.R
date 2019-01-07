f528ba0860131e0c68f13a9d5f478e85fa1e06b9
Nicky Poison <nickpoison@hotmail.com>
2019-01-06 23:18:46 -0500

    ssm.R

--- R/ssm.R
+++ R/ssm.R
@@ -7,7 +7,6 @@ P00 = var(y)
 num = length(y)
 B = A
 A = array(A, dim=c(1,1,num))
-#
 # function to calc likelihood
 Linn=function(para){
   if (fixphi){ phi=phi; alpha = para[1]; sigw = para[2];  sigv = para[3] 
@@ -15,7 +14,6 @@ Linn=function(para){
  kf = Kfilter1(num,y,A,x00,P00,phi,alpha,0,sigw,sigv,rep(1,num))
  return(kf$like) 
 }
-#
 # Estimation
 if (fixphi){ init.par = c(alpha,sigw,sigv) 
  } else {init.par = c(phi,alpha,sigw,sigv)
@@ -27,9 +25,9 @@ if (fixphi){ phat = c(phi, est$par)
 } else {phat = est$par}
 # run filter /smoother with estimates
 ks = Ksmooth1(num,y,A,x00,P00,phat[1],phat[2],0,phat[3],phat[4],rep(1,num))
-Xp = ts(as.vector(ks$xp), start=strt, freq=frq) + phat[2]
+Xp = ts(as.vector(ks$xp), start=strt, freq=frq) 
 Pp = ts(as.vector(ks$Pp), start=strt, freq=frq)
-Xs = ts(as.vector(ks$xs), start=strt, freq=frq) + phat[2]
+Xs = ts(as.vector(ks$xs), start=strt, freq=frq) 
 Ps = ts(as.vector(ks$Ps), start=strt, freq=frq)
 estimate = est$par 
 u = cbind(estimate, SE) 
