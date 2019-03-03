bd850820508c6bb9d653c03f72ab6aa4d88563f9
Nicky Poison <nickpoison@hotmail.com>
2019-03-03 18:12:09 -0500

    tsplot

--- R/tsplot.R
+++ R/tsplot.R
@@ -1,13 +1,12 @@
-tsplot <- function(x, y = NULL, main=NULL, ylab=NULL, xlab='Time', type='l', margins=.25, ... ){
+tsplot <- function(x, y = NULL, main=NULL, ylab=NULL, xlab='Time', type='l', margins=.25, nxm=4, nym=4, ... ){
   par = graphics::par
   plot = graphics::plot
-  grid = graphics::grid
   lines = graphics::lines
   topper = ifelse(is.null(main), 0, .5)  
   if(is.null(ylab)) {ylab = ifelse(is.null(y), deparse(substitute(x)), deparse(substitute(y)))}
   par(mar=c(2.5,2.5,1+topper,.5)+margins, mgp=c(1.6,.6,0), cex.main=1.2)
   plot(x, y, type='n', main=main, ylab=ylab, xlab=xlab, ... ) 
-  grid(lty=1, col=gray(.9))
+  Grid(nxm=nxm, nym=nym)
   lines(x, y, type=type, ... )
   box()
   }
