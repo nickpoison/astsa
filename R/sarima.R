a01b8fe57aac61cbdb23dd5f66173d8d89dcb325
Nicky Poison <nickpoison@hotmail.com>
2019-03-10 14:45:45 -0400

    fixed in sarima.R

--- R/sarima.R
+++ R/sarima.R
@@ -91,17 +91,19 @@ if(details){
     on.exit(par(old.par)) 
 }	
 #  end new tsdiag
-   coefs = fitit$coef[abs(fitit$coef)>0]  
+   if (is.null(fixed)) {coefs=fitit$coef
+   } else { coefs = fitit$coef[is.na(fixed)]
+   }  
    dfree = fitit$nobs-length(coefs) 
-  t.value=coefs/sqrt(diag(fitit$var.coef)) 
-  p.two = stats::pf(t.value^2, df1=1, df2=dfree, lower.tail = FALSE)   
-  ttable = cbind(Estimate=coefs, SE=sqrt(diag(fitit$var.coef)), t.value, p.value=p.two)
-  ttable= round(ttable,4)
-  k = length(coefs) 
-  BIC  = (log(n)*k - 2*fitit$loglik)/n 
-  AIC  = (2*k - 2*fitit$loglik)/n
-  AICc = (n*AIC + ( (2*k^2+2*k)/(n-k-1) ))/n
-  list(fit=fitit, degrees_of_freedom=dfree, ttable=ttable, AIC=AIC, AICc=AICc, BIC=BIC)
+   t.value=coefs/sqrt(diag(fitit$var.coef)) 
+   p.two = stats::pf(t.value^2, df1=1, df2=dfree, lower.tail = FALSE)   
+   ttable = cbind(Estimate=coefs, SE=sqrt(diag(fitit$var.coef)), t.value, p.value=p.two)
+   ttable= round(ttable,4)
+   k = length(coefs) 
+   BIC  = (log(n)*k - 2*fitit$loglik)/n 
+   AIC  = (2*k - 2*fitit$loglik)/n
+   AICc = (n*AIC + ( (2*k^2+2*k)/(n-k-1) ))/n
+   list(fit=fitit, degrees_of_freedom=dfree, ttable=ttable, AIC=AIC, AICc=AICc, BIC=BIC)
 }
 
 
