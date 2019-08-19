ecb9e7f83a8a238c998bef6b290e013786dae7a2
nickpoison <nickpoison@hotmail.com>
2019-08-19 17:49:53 -0400

    tsplot.R

--- R/tsplot.R
+++ R/tsplot.R
@@ -1,14 +1,32 @@
 tsplot <- function(x, y = NULL, main=NULL, ylab=NULL, xlab='Time', type='l', margins=.25, 
-                   minor=TRUE, nxm=2, nym=2, ... ){
-  par = graphics::par
-  plot = graphics::plot
+                    ncolm=1, minor=TRUE, nxm=2, nym=2, col=1, ... ){
+  par   = graphics::par
+  plot  = graphics::plot
   lines = graphics::lines
-  topper = ifelse(is.null(main), 0, .5)  
+  nser  = max(NCOL(x), NCOL(y))
+  topper = ifelse(is.null(main), 0, .5) 
+  oo<-options()  
+  if (nser == 1) {
   if(is.null(ylab)) {ylab = ifelse(is.null(y), deparse(substitute(x)), deparse(substitute(y)))}
-  par(mar=c(2.5,2.5,1+topper,.5)+margins, mgp=c(1.6,.6,0), cex.main=1.2)
+  op<-par(mar=c(2.5,2.5,1+topper,.5)+margins, mgp=c(1.6,.6,0), cex.main=1.2)
   plot(x, y, type='n', main=main, ylab=ylab, xlab=xlab, ... ) 
   Grid(minor=minor, nxm=nxm, nym=nym)
-  lines(x, y, type=type, ... )
+  lines(x, y, type=type, col=col, ... ) 
   box()
-  }
-  
+  } else {
+  prow = ceiling(nser/ncolm)
+  oo<-options(warn=-1)
+  culer = matrix(col, nser)
+  op<-par(mfrow = c(prow, ncolm), mar = c(2.5, 2.5, 1, .5), mgp = c(1.6,.6, 0), cex.lab=1.1, oma = c(0,0,3*topper,0))
+  if (is.null(y)) { ylab=colnames(as.matrix(x)) } else { ylab=colnames(as.matrix(y))} 
+  if (is.null(ylab)) { ylab = NA }
+  for (h in 1:nser) {
+    if(is.null(y)) {tsplot(x[,h], ylab=ylab[h], col=culer[h,], ...)
+	} else {
+	tsplot(x, y[,h], ylab=ylab[h], col=culer[h,], ...)
+    }
+    }  	
+   mtext(text=main, line=-.5, outer=TRUE, font=2) 
+   } 
+ par(op); options(oo)
+}
