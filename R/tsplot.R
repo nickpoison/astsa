526f11196c6bb964ed6d096c4f2fa2ae41f27d7d
nickpoison <nickpoison@hotmail.com>
2019-09-28 21:41:58 -0400

    fixing tsplot

--- R/tsplot.R
+++ R/tsplot.R
@@ -5,19 +5,18 @@ tsplot <- function(x, y = NULL, main=NULL, ylab=NULL, xlab='Time', type='l', mar
   lines = graphics::lines
   nser  = max(NCOL(x), NCOL(y))
   topper = ifelse(is.null(main), 0, .5) 
-  ## oo<-options()  
+  old.par <- par(no.readonly = TRUE)  
   if (nser == 1) {
   if(is.null(ylab)) {ylab = ifelse(is.null(y), deparse(substitute(x)), deparse(substitute(y)))}
-  op<-par(mar=c(2.5,2.5,1+topper,.5)+margins, mgp=c(1.6,.6,0), cex.main=1.2)
+  par(mar=c(2.5,2.5,1+topper,.5)+margins, mgp=c(1.6,.6,0), cex.main=1.2)
   plot(x, y, type='n', main=main, ylab=ylab, xlab=xlab, ... ) 
   Grid(minor=minor, nxm=nxm, nym=nym)
   lines(x, y, type=type, col=col, ... ) 
   box()
   } else {
   prow = ceiling(nser/ncolm)
-  ## oo<-options(warn=-1)
   culer = matrix(col, nser)
-  op<-par(mfrow = c(prow, ncolm), mar = c(2, 2, .5, .25)+margins, mgp = c(1.6,.6, 0), cex.lab=1.1, oma = c(0,0,3*topper,0))
+  par(mfrow = c(prow, ncolm), mar = c(2, 2, .5, .25)+margins, mgp = c(1.6,.6, 0), cex.lab=1.1, oma = c(0,0,3*topper,0))
   if (is.null(y)) { ylab=colnames(as.matrix(x)) } else { ylab=colnames(as.matrix(y))} 
   if (is.null(ylab)) { ylab = NA }
   for (h in 1:nser) {
@@ -28,5 +27,5 @@ tsplot <- function(x, y = NULL, main=NULL, ylab=NULL, xlab='Time', type='l', mar
     }  	
    mtext(text=main, line=-.5, outer=TRUE, font=2) 
    } 
- par(op) ## options(oo)
+on.exit(par(old.par))    
 }
