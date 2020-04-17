952c3a5d34687f87463f91fd7e6551403dcd04f5
nickpoison <nickpoison@hotmail.com>
2020-04-17 19:52:08 -0400

    covid-19

--- R/tsplot.R
+++ R/tsplot.R
@@ -1,5 +1,5 @@
  tsplot <- function(x, y = NULL, main=NULL, ylab=NULL, xlab='Time', type=NULL, 
-                   margins=.25, ncolm=1, byrow=TRUE, minor=TRUE, nxm=2, nym=2, 
+                   margins=.25, ncolm=1, byrow=FALSE, minor=TRUE, nxm=2, nym=2, 
 				   col=1, ...)
 {
   par   = graphics::par
@@ -10,24 +10,24 @@
   type0 <- 'n' 
   type1 <- ifelse(is.null(type), 'l', type)
   if (nser == 1) {
-  if(is.null(ylab)) {ylab = ifelse(is.null(y), deparse(substitute(x)), deparse(substitute(y)))}
-  par(mar=c(2.5,2.5,1+topper,.5)+margins, mgp=c(1.6,.6,0), cex.main=1.2)
-  plot(x, y, type = type0, axes=FALSE, ann=FALSE, main=NULL, ... )
-  Grid(minor=minor, nxm=nxm, nym=nym)
-  par(new=TRUE)
-  plot(x, y, type=type1, main=main, ylab=ylab, xlab=xlab, col=col, ... ) 
-  box()
+   if(is.null(ylab)) {ylab = ifelse(is.null(y), deparse(substitute(x)), deparse(substitute(y)))}
+   par(mar=c(2.5,2.5,1+topper,.5)+margins, mgp=c(1.6,.6,0), cex.main=1.2)
+   plot(x, y, type = type0, axes=FALSE, ann=FALSE, main=NULL, ... )
+   Grid(minor=minor, nxm=nxm, nym=nym)
+   par(new=TRUE)
+   plot(x, y, type=type1, main=main, ylab=ylab, xlab=xlab, col=col, ... ) 
+   box()
   } else {
-  prow = ceiling(nser/ncolm)
-  culer = matrix(col, nser)
-  if(byrow){
-  par(mfrow = c(prow, ncolm), mar = c(2, 2, .5, .25)+margins, mgp = c(1.6,.6, 0), cex.lab=1.1, oma = c(0,0,3*topper,0))
-  } else {
-  par(mfcol = c(prow, ncolm), mar = c(2, 2, .5, .25)+margins, mgp = c(1.6,.6, 0), cex.lab=1.1, oma = c(0,0,3*topper,0))
-  }
-  if (is.null(y)) { ylab=colnames(as.matrix(x)) } else { ylab=colnames(as.matrix(y))} 
-  if (is.null(ylab)) { ylab = NA }
-  for (h in 1:nser) {
+   prow = ceiling(nser/ncolm)
+   culer = matrix(col, nser)
+   if(byrow){
+   par(mfrow = c(prow, ncolm), mar = c(2, 2, .5, .25)+margins, mgp = c(1.6,.6, 0), cex.lab=1.1, oma = c(0,0,3*topper,0))
+   } else {
+   par(mfcol = c(prow, ncolm), mar = c(2, 2, .5, .25)+margins, mgp = c(1.6,.6, 0), cex.lab=1.1, oma = c(0,0,3*topper,0))
+   }
+   if (is.null(y)) { ylab=colnames(as.matrix(x)) } else { ylab=colnames(as.matrix(y))} 
+   if (is.null(ylab)) { ylab = NA }
+   for (h in 1:nser) {
     if(is.null(y)) {tsplot(x[,h], ylab=ylab[h], col=culer[h,], type=type, xlab=xlab, ...)
 	} else {
 	tsplot(x, y[,h], ylab=ylab[h], col=culer[h,], type=type, xlab=xlab, ...)
