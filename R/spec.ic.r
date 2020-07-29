f1df1012babf450bee559862509906c8aec36daf
Nicky Poison <nickpoison@hotmail.com>
2020-07-29 19:43:20 -0400

    Update spec.ic.r

--- R/spec.ic.r
+++ R/spec.ic.r
@@ -2,23 +2,26 @@ spec.ic =
 function(data, BIC=FALSE, order.max=30, main=NULL, plot=TRUE, 
            detrend=FALSE, method=NULL, ...){
   if (is.null(method)) {method='yw'}	   
-  nme1 = paste("Series:", deparse(substitute(data)))	
+  nme1 = paste("Series:", deparse(substitute(data)))
+  nme2 = ifelse(BIC,'BIC','AIC')  
   if (detrend) { data = resid(lm(data~time(data), na.action=NULL)); dmean = FALSE
-  } else { dmean = TRUE } 		  
-  aic  = as.vector(stats::ar(data, order=order.max, aic=TRUE, method=method, demean=dmean)$aic) 
+  } else { dmean = TRUE } 	
+    u  = stats::ar(data, order=order.max, aic=TRUE, method=method, demean=dmean)	
+  aic  = as.vector(u$aic) 
   bic  = aic - 2*(0L:order.max) + (0L:order.max)*log(length(data)) 
   bic  = bic - min(bic)  
   kmin = ifelse(BIC, which.min(bic)-1, which.min(aic)-1)
+  if (kmin < 1) { 
+    kmin=NULL 
+    arma.spec(var.noise=var(data), main=paste(nme1,"  |  ",nme2," order = ",0), ...)
+	}
   u    = stats::ar(data, order=kmin, aic=FALSE, method=method)
   u2   = stats::spec.ar(u, plot=FALSE)
- if (plot){
-  nme2 = ifelse(BIC,'BIC','AIC')
+ if (plot & !is.null(kmin)){
   if (is.null(main)) {main = paste(nme1,"  |  ",nme2," order = ", kmin, sep="")}
-  tsplot(u2$freq, u2$spec, ylab='AR Spectrum', xlab='Frequency', margins=.5, 
-         main=main, cex.axis=.85, las=0, ...)
-  }		
+  arma.spec(ar=u$ar, var.noise=var(data), main=main, ...)
+  }	
   out1 = cbind(ORDER=(0L:order.max), AIC=aic, BIC=bic)
   out2 = cbind(freq=u2$freq, spec=u2$spec) 
  return(invisible(list(out1, out2)))
-}
-  
+} 
