038ebccb4893fe6cfbe1ce41ab5fa948ec4a294f
Nicky Poison <nickpoison@hotmail.com>
2020-07-30 22:46:44 -0400

    Update spec.ic.r

--- R/spec.ic.r
+++ R/spec.ic.r
@@ -6,16 +6,17 @@ function(data, BIC=FALSE, order.max=30, main=NULL, plot=TRUE,
   nme2 = ifelse(BIC,'BIC','AIC')  
   if (detrend) { data = resid(lm(data~time(data), na.action=NULL)); dmean = FALSE
   } else { dmean = TRUE } 	
-    u  = stats::ar(data, order=order.max, aic=TRUE, method=method, demean=dmean)	
+  u    = stats::ar(data, order=order.max, aic=TRUE, method=method, demean=dmean)	
   aic  = as.vector(u$aic) 
   bic  = aic - 2*(0L:order.max) + (0L:order.max)*log(length(data)) 
   bic  = bic - min(bic)  
   kmin = ifelse(BIC, which.min(bic)-1, which.min(aic)-1)
-  if (kmin < 1) { 
-    kmin=NULL 
-	if (is.null(main)) {main=paste(nme1,"  |  ",nme2," order = ",0)}
+  if (kmin < 1) { kmin=NULL 
+	if (plot){
+    if (is.null(main)) {main=paste(nme1,"  |  ",nme2," order = ",0)}
     arma.spec(var.noise=var(data), main=main, ...)
 	}
+  }	
   u    = stats::ar(data, order=kmin, aic=FALSE, method=method, demean=dmean)
   u2   = stats::spec.ar(u, plot=FALSE)
  if (plot & !is.null(kmin)){
