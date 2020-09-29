31a7504a07befe9bee12e32f5f0795652ff67b69
Nickpoison <nickpoison@hotmail.com>
2020-09-29 13:23:10 -0400

    sarima simulation

--- R/polyMul.R
+++ R/polyMul.R
@@ -2,9 +2,9 @@ polyMul <-
 function(p,q)
   { 
     p <- rev(p)
-n <- length(p)
+    n <- length(p)
     q <- rev(q)
-m <- length(q)
+    m <- length(q)
     k <- n+m-1
     raw <- c(p, rep(c(rep(0, n+m-1), p), n+m-2))
     P <- matrix(raw, 2*n+m-2)[1:(n+m-1),]
