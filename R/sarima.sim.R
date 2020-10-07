dd3b0ef7479ad1b8286dac7c9d3b1be4576fba2b
Nicky Poison <nickpoison@hotmail.com>
2020-10-07 19:14:42 -0400

    sarima.sim

--- R/sarima.sim.R
+++ R/sarima.sim.R
@@ -17,9 +17,7 @@ function(ar=NULL, d=0, ma=NULL, sar=NULL, D=0, sma=NULL, S=NULL,
    SAR[1] = 1
    SAR[seq(S, Po*S,by=S)] = -sar
     minroots <- min(Mod(SAR))
-    if (minroots <= 1) 
-      stop("model is not causal")
-    }
+    if (minroots <= 1) { stop("model is not causal") }
    if (po>1) {
     AR = c(1,-ar) 
    } else {
@@ -35,9 +33,7 @@ function(ar=NULL, d=0, ma=NULL, sar=NULL, D=0, sma=NULL, S=NULL,
    SMA[1] = 1
    SMA[seq(S, Qo*S,by=S)] = sma
     minroots <- min(Mod(SMA))
-     if (minroots <= 1) 
-      stop("model is not invertible")
-    }
+     if (minroots <= 1) { stop("model is not invertible") }
    if (qo>1) {
     MA = c(1,ma) 
    } else {
