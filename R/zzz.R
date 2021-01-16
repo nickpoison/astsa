371da997d50b578da1c25b547492606c93a995db
Nicky Poison <nickpoison@hotmail.com>
2021-01-15 22:19:16 -0500

    colors

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 .onAttach <- function(library, pkg){ 
-			 palette(c("black","#F8766D","#00BA38","#1773CC","#00BFC4","#BA23AB","#B79F00","gray62"))
+			 palette(c("black","#F8766D","#00BA38","#1773CC","#00AFC4","#BA23AB","#B79F00","gray62"))
 			 }
 			 
 .onDetach <- function(library, pkg){ 
