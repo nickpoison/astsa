d9a38535767a8a47e844f7632c1516bf8adf7efe
Nicky Poison <nickpoison@hotmail.com>
2021-01-17 13:08:23 -0500

    gold 2 strong orange

--- R/astsa.col.r
+++ R/astsa.col.r
@@ -1,4 +1,5 @@
 astsa.col <- function(alpha=1) {  
-     u <- c("black","#F6483C","#00BA38","#1773CC","#00AFC4","#BA23AB","#B79F00","gray62")
+     u <- c("black","#F6483C","#00BA38","#1773CC","#00AFC4","#BA23AB","#C47700","gray62")
 	 grDevices::adjustcolor(u, alpha=alpha)
 	 }
+	 
--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 .onAttach <- function(library, pkg){ 
-			 palette(c("black","#F6483C","#00BA38","#1773CC","#00AFC4","#BA23AB","#B79F00","gray62"))
+			 palette(c("black","#F6483C","#00BA38","#1773CC","#00AFC4","#BA23AB","#C47700","gray62"))
 			 }
 			 
 .onDetach <- function(library, pkg){ 
