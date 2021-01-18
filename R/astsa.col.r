2a7d9c8b384ae1f48e0a1e8541a6f6e0a248df9c
Nicky Poison <nickpoison@hotmail.com>
2021-01-18 13:27:30 -0500

    colors

--- R/astsa.col.r
+++ R/astsa.col.r
@@ -1,4 +1,4 @@
 astsa.col <- function(alpha=1) {  
-     u <- c("black","#F6483C","#00BA38","#1773CC","#0D9AC0","#BA23AB","#C47700","gray62")
+     u <- c("black","#F6483C","#00BA38","#1874cd","#0D9AC0","#cd1874","#C47700","gray62")
 	 grDevices::adjustcolor(u, alpha=alpha)
 	 }
--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,7 @@
 .onAttach <- function(library, pkg){ 
-			 palette(c("black","#F6483C","#00BA38","#1773CC","#0D9AC0","#BA23AB","#C47700","gray62"))
+			 palette(c("black","#F6483C","#00BA38","#1874cd","#0D9AC0","#cd1874","#C47700","gray62"))
 			 }
 			 
 .onDetach <- function(library, pkg){ 
 			 palette("default")
-			 }
+			 }	 
