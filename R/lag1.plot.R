cee128d63baea70f05285f034bd937824dc2c88f
Nicky Poison <nickpoison@hotmail.com>
2021-02-17 13:06:48 -0500

    pretty

--- R/lag1.plot.R
+++ R/lag1.plot.R
@@ -1,5 +1,5 @@
 lag1.plot <-
-function(series,max.lag=1,corr=TRUE,smooth=TRUE,col=gray(.1), ...){ 
+function(series, max.lag=1, corr=TRUE, smooth=TRUE, col=gray(.1), ...){ 
    #
    as.ts = stats::as.ts
    par = graphics::par
@@ -16,14 +16,14 @@ function(series,max.lag=1,corr=TRUE,smooth=TRUE,col=gray(.1), ...){
    pcol=ceiling(max.lag/prow)
    a=stats::acf(series,max.lag,plot=FALSE)$acf[-1]
    old.par <- par(no.readonly = TRUE)
-   par(mfrow=c(prow,pcol), mar=c(2.5, 2.5, 1, 1), mgp=c(1.6,.6,0), cex.main=1, font.main=1)
+   par(mfrow=c(prow,pcol))
   for(h in 1:max.lag){                       
-   plot(stats::lag(series,-h), series, xy.labels=FALSE, main=paste(name1,h,")",sep=""), ylab=name2, xlab="", col=col, panel.first=Grid(), ...) 
+   tsplot(stats::lag(series,-h), series, type='p', xy.labels=FALSE, xlab=paste(name1,h,")",sep=""), ylab=name2, col=col, ...) 
     if (smooth==TRUE) 
     lines(stats::lowess(ts.intersect(stats::lag(series,-h),series)[,1],
-                 ts.intersect(stats::lag(series,-h),series)[,2]), col="red")
+                 ts.intersect(stats::lag(series,-h),series)[,2]), col=2)
     if (corr==TRUE)
-    legend("topright", legend=round(a[h], digits=2), text.col ="blue", bg="white", adj=.25, cex = 0.85)
+    legend("topright", legend=round(a[h], digits=2), text.col=4, bg="white", adj=.25, cex = 0.85)
    on.exit(par(old.par))
    }
 }
--- R/lag2.plot.R
+++ R/lag2.plot.R
@@ -18,14 +18,14 @@ function(series1,series2,max.lag=0,corr=TRUE,smooth=TRUE,col=gray(.1), ...){
    pcol=ceiling(m1/prow)
    a=stats::ccf(series1,series2,max.lag,plot=FALSE)$acf
    old.par <- par(no.readonly = TRUE)
-   par(mfrow=c(prow,pcol),  mar=c(2.5, 2.5, 1, 1), mgp=c(1.6,.6,0), cex.main=1, font.main=1)
+   par(mfrow=c(prow,pcol))#,  mar=c(2.5, 2.5, 1, 1), mgp=c(1.6,.6,0), cex.main=1, font.main=1)
    for(h in 0:max.lag){                   
-   plot(stats::lag(series1,-h), series2, xy.labels=FALSE, main=paste(name1,h,")",sep=""), ylab=name2, xlab="", col=col, panel.first=Grid(), ...) 
+   tsplot(stats::lag(series1,-h), series2, xy.labels=FALSE, type='p', xlab=paste(name1,h,")",sep=""), ylab=name2, col=col, ...) 
     if (smooth==TRUE) 
     lines(stats::lowess(ts.intersect(stats::lag(series1,-h),series2)[,1],
-                 ts.intersect(stats::lag(series1,-h),series2)[,2]), col="red")
+                 ts.intersect(stats::lag(series1,-h),series2)[,2]), col=2)
     if (corr==TRUE)
-    legend("topright", legend=round(a[m1-h], digits=2), text.col ="blue", bg="white", adj=.25, cex = 0.85)             
+    legend("topright", legend=round(a[m1-h], digits=2), text.col =4, bg="white", adj=.25, cex = 0.85)             
    on.exit(par(old.par))
    }
 }
