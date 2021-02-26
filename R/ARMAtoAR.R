c5fcdcb50daef8c1527936ada9b298628d019fb3
Nicky Poison <nickpoison@hotmail.com>
2021-02-26 13:26:06 -0500

    ARMAtoAR no longer rounds

--- R/ARMAtoAR.R
+++ R/ARMAtoAR.R
@@ -2,5 +2,5 @@ ARMAtoAR <- function(ar = 0, ma = 0, lag.max=20){
              ARMAtoMA <- stats::ARMAtoMA
              arinv <- -ma
              mainv <- -ar
-             return(round(ARMAtoMA(arinv, mainv, lag.max), 3))
+             return(ARMAtoMA(arinv, mainv, lag.max))
 }
