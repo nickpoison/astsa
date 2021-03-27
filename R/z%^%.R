6a1e4975a1ffbc3a12d98ac349e90211247bcfad
Nicky Poison <nickpoison@hotmail.com>
2021-03-26 22:45:44 -0400

    matrix power

---
+++ R/z%^%.R
@@ -0,0 +1,16 @@
+`%^%` <-
+function(A, power){
+   if (!is.matrix(A))  stop("object not a matrix") 
+   if (abs( nrow(A) - ncol(A) ) > 0 ) stop("matrix must be square") 
+   if (power == 0) return(diag(1, nrow(A)))
+   if (power == 1) return(A)
+   if (power < 0 ) {
+      singtest <- "matrix" %in% class(try(solve(A),silent=TRUE))
+      if (!singtest) stop("matrix nonsinular")
+	  }
+   if (isSymmetric(A)) {
+    with(eigen(A), vectors %*% (values^power * t(vectors)))
+   } else {
+    with(eigen(A), vectors %*% (values^power * solve(vectors))) 
+   }
+}
