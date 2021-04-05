4e48faa82ef577eca633f819c7d93a616caba845
Nicky Poison <nickpoison@hotmail.com>
2021-04-05 14:51:42 -0400

    astsa_1.12.9.tar.gz

--- R/test.linear.R
+++ R/test.linear.R
@@ -2,7 +2,7 @@ test.linear <-
 function(series, color=TRUE, detrend=FALSE){
 if (NCOL(series)>1){stop("univariate series only")}
 x = series - mean(series)
-N = length(x)       # data length
+N = length(x)       #  data length
 L = floor(N^.49)    # block width
 P = floor(N/L)      # number of blocks
 if (detrend) {
@@ -19,7 +19,7 @@ X = matrix(NA, L, P)
  }
 X  = X[-1,]  # get rid of k=0 freq; now X[-k,p] = X[L-k,p]
 S  = rowSums(Mod(X)^2)/N
-L1 = floor((L-1)/2)
+L1 = floor((L-1)/2)       
 G  = matrix(as.complex(0), L1,L1)
   for (k1 in 1:L1){ 
    for (k2 in 1:L1){ u = 0
@@ -29,7 +29,7 @@ G  = matrix(as.complex(0), L1,L1)
    }
   }
 Test   = 2*N^(-1+2*.49)*abs(G)^2
-Tst    = Test     # this is just a placeholder
+Tst    = Test     # will eventually hold the pvalues
 lamhat = mean(Test)
  for (k1 in 1:L1){ 
   for (k2 in 1:L1){
@@ -46,8 +46,7 @@ filled.contour(u,u,Tst,  col=clrs, nlevels=50, zlim=c(0,1),
 mtext(expression(omega[1]), side=1, line=2,    adj=.4, cex=1.2)  
 mtext(expression(omega[2]), side=2, line=2.75, adj=.5, cex=1.2, las=1)
 title("Normalized BiSpectrum", adj =.35, line=1, cex.main=1.2) 
-# values
-test.out = list(prob=Tst)
-return(invisible(test.out))
+# output
+invisible(Tst)
 }
 
