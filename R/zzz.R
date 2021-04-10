50341dff1047732fa230ca16f07d942808cc6622
Nicky Poison <nickpoison@hotmail.com>
2021-04-10 18:28:12 -0400

    clean up

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,7 @@
-.onAttach <- function(library, pkg){ 
+.onAttach <- function(){ 
 			 palette(c("black","#F6483C","#00BA38","#1874cd","#0D9AC0","#cd1874","#CD7118","gray62"))
 			 }
 			 
-.onDetach <- function(library, pkg){ 
+.onDetach <- function(){ 
 			 palette("default")
 			 }	 
