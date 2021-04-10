585bed1d2da9d8283ca939876e22972299991a32
Nicky Poison <nickpoison@hotmail.com>
2021-04-10 19:26:11 -0400

    zed

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,7 @@
-.onAttach <- function(){ 
+.onAttach <- function(library, pkg){ 
 			 palette(c("black","#F6483C","#00BA38","#1874cd","#0D9AC0","#cd1874","#CD7118","gray62"))
 			 }
 			 
-.onDetach <- function(){ 
+.onDetach <- function(library, pkg){ 
 			 palette("default")
 			 }	 
