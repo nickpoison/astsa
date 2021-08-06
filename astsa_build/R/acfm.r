ee9493cb17e7a5cff56f3178656263ac9d72dc55
Nicky Poison <nickpoison@hotmail.com>
2021-08-06 00:46:16 -0400

    v1.13.2

--- astsa_build/R/acfm.r
+++ astsa_build/R/acfm.r
@@ -22,20 +22,21 @@ lowr = max(lowr, -1)
 uppr = max(as.vector(u$acf), na.rm = TRUE) + .05
 uppr = min(uppr, 1)
 
-U = (-1/num) + (2/sqrt(num))
-L = (-1/num) - (2/sqrt(num))
+
 
 old.par <- par(no.readonly = TRUE)
 par(mfrow=c(nser,nser), mar=c(1,1,.5,.5), mgp=c(1.6,.6,0), oma=c(0,2,2,0), cex.main=1)
  Xlab = ifelse(frequency>1, paste('LAG', expression('\u00F7'), frequency), 'LAG')
  for (i in 1:nser){ 
   for (j in 1:nser){ 
+     U = (-1/num)*(i==j) + (2/sqrt(num))
+     L = (-1/num)*(i==j) - (2/sqrt(num))
     tsplot(u$lag[,i,j], u$acf[,i,j], type='h', ylab="", main=NULL, xlab=Xlab, ylim=c(lowr,uppr))
      abline(h = c(0, L, U), lty = c(1, 2, 2), col = c(8, 4, 4))       
      txt2 = ifelse(j==1,u$snames[i],"") 
-      mtext(txt2, side=2, font=2, line=2, cex=.75*(num+1)/num)
+      mtext(txt2, side=2, font=2, line=2, cex=.65*(nser+1)/nser)
      txt3 = ifelse (i==1,u$snames[j],"") 
-       mtext(txt3, side=3, font=2, line=.75, cex=.75*(num+1)/num)
+       mtext(txt3, side=3, font=2, line=.75, cex=.65*(nser+1)/nser)
       if(i==j) box(col=1)
   }   
  } 
@@ -43,6 +44,7 @@ mtext('Leads', side=3, line=.65, outer=TRUE, cex=.9)
 mtext('Lags',  side=2, line=.65, outer=TRUE, cex=.9)  
 par(old.par)
 }
+
 
 
 
