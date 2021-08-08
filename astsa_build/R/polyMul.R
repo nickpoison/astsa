193967dc76c0fa8bfdaf18afda88e125cbb08dc6
Nicky Poison <nickpoison@hotmail.com>
2021-08-08 10:24:29 -0400

    clean up

--- astsa_build/R/polyMul.R
+++ astsa_build/R/polyMul.R
@@ -1,13 +1,12 @@
 polyMul <-
 function(p,q)
   { 
-    p <- rev(p)
-    n <- length(p)
-    q <- rev(q)
-    m <- length(q)
-    k <- n+m-1
+    p   <- rev(p)
+    n   <- length(p)
+    q   <- rev(q)
+    m   <- length(q)
     raw <- c(p, rep(c(rep(0, n+m-1), p), n+m-2))
-    P <- matrix(raw, 2*n+m-2)[1:(n+m-1),]
-    Q <- matrix(c(q, rep(0,n-1)), ncol=1)
+    P   <- matrix(raw, 2*n+m-2)[1:(n+m-1),]
+    Q   <- matrix(c(q, rep(0,n-1)), ncol=1)
     return(rev(P%*%Q)) 
   }
