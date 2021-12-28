debb545e343226e55da5dbeffa44e98d42277e26
Nicky Poison <nickpoison@hotmail.com>
2021-12-28 13:42:35 -0500

    v1.14.3

--- astsa_build/R/arma.spec.R
+++ astsa_build/R/arma.spec.R
@@ -1,6 +1,6 @@
 arma.spec <-
 function(ar=0, ma=0, var.noise=1, n.freq=500,  main='from specified model', 
-          frequency=1, ...)
+          frequency=1, ylim=NULL, ...)
 {  
      check <- 0
     # check causality
@@ -13,24 +13,29 @@ function(ar=0, ma=0, var.noise=1, n.freq=500,  main='from specified model',
      if(any(abs(z.ma) <= 1)) {cat("WARNING: Model Not Invertible", "\n"); check <- check + 1}
      if (check > 0) stop("Try Again")
     #
-    ar.order <- length(ar)
-    ma.order <- length(ma) 
+     ar.order <- length(ar)
+     ma.order <- length(ma)
     # check (near) parameter redundancy [i.e. are any roots (approximately) equal]  
        for (i in 1:ar.order) {
-       if ( (ar[1] == 0 && ar.order == 1) || (ma[1] == 0 && ma.order ==1) )  break
+       if ( (ar[1] == 0 && ar.order == 1) || (ma[1] == 0 && ma.order == 1) )  break
        if(any(abs(z.ar[i]-z.ma[1:ma.order]) < 1e-01)) {cat("WARNING: Parameter Redundancy", "\n"); break}
        }
     #
     freq <- seq.int(0, 0.5, length.out = n.freq)
-            cs.ar <- outer(freq, 1:ar.order, function(x, y) cos(2 * pi * x * y)) %*% ar
-            sn.ar <- outer(freq, 1:ar.order, function(x, y) sin(2 * pi * x * y)) %*% ar
-            cs.ma <- outer(freq, 1:ma.order, function(x, y) cos(2 * pi * x * y)) %*% -ma
-            sn.ma <- outer(freq, 1:ma.order, function(x, y) sin(2 * pi * x * y)) %*% -ma                      
+      cs.ar <- outer(freq, 1:ar.order, function(x, y) cos(2 * pi * x * y)) %*% ar
+      sn.ar <- outer(freq, 1:ar.order, function(x, y) sin(2 * pi * x * y)) %*% ar
+      cs.ma <- outer(freq, 1:ma.order, function(x, y) cos(2 * pi * x * y)) %*% -ma
+      sn.ma <- outer(freq, 1:ma.order, function(x, y) sin(2 * pi * x * y)) %*% -ma                      
     spec <- var.noise*((1 - cs.ma)^2 + sn.ma^2)/((1 - cs.ar)^2 + sn.ar^2)
+    
     spg.out <- list(freq=freq*frequency, spec=spec)
     Ylab = 'spectrum'
     Xlab = ifelse(frequency>1, paste('frequency', expression('\u00D7'), frequency), 'frequency')
-   tsplot(freq*frequency, spec, xlab=Xlab, ylab=Ylab, main=main,  ...)
+    m1   = min(spec)
+    m2   = max(spec)
+    yspread = (m2-m1)/sqrt(var.noise) 
+    if (is.null(ylim) & yspread < 4) ylim = c(max(m1-2,0.1), m2+2)
+   tsplot(freq*frequency, spec, xlab=Xlab, ylab=Ylab, main=main, ylim=ylim, ...)
    return(invisible(spg.out))
 }
 
