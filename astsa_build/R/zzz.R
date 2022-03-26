979db1d3886c5a703b5489d7c17632702ac7fd31
Nicky Poison <nickpoison@hotmail.com>
2022-03-26 15:53:01 -0400

    master

--- astsa_build/R/zzz.R
+++ astsa_build/R/zzz.R
@@ -1,7 +1,7 @@
-.onAttach <- function(libname, pkgname){ 
+.onLoad <- function(libname, pkgname){ 
 			 palette(c("black","#F6483C","#00BA38","#1874cd","#0D9AC0","#cd1874","#CD7118","gray62"))
 			 }
 			 
-.onDetach <- function(libpath){ 
+.onUnload <- function(libpath){ 
 			 palette("default")
 			 }	 
