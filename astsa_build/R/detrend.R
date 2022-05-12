c428107d28a39960cbf7e6011cd888810639fa41
Nicky Poison <nickpoison@hotmail.com>
2022-05-12 14:51:01 -0400

    v1.15+

--- astsa_build/R/detrend.R
+++ astsa_build/R/detrend.R
@@ -1,5 +1,6 @@
 detrend <-
 function(series, order=1, lowess=FALSE, lowspan=2/3){
+  if (NCOL(series) > 1) stop("univariate time series only")
     if (lowess) { 
       u = stats::lowess(series, f=lowspan)
       return(series-u$y)  
