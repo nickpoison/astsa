0ddd0b2af05c2d335cbd71049a8860360467b45c
Nicky Poison <nickpoison@hotmail.com>
2022-08-16 23:00:23 -0400

    v1.16

--- astsa_build/R/ESS.R
+++ astsa_build/R/ESS.R
@@ -1,7 +1,7 @@
 ESS <-
 function(trace, tol = 1e-8){
   if (NCOL(trace) > 1) stop('univariate input only')
-  v = spec.ic(trace, BIC=TRUE, plot=FALSE)
+  v = spec.ic(trace, BIC=TRUE, order.max=8, plot=FALSE)
   spec0 = as.numeric(v[[2]][1,2])
   if (spec0 <= tol){
     return(0) 
