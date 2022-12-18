487d819af08cb63b168d9d5ea9ccd26b4b6fd83f
David Stoffer <stoffer@pitt.edu>
2022-12-17 20:40:19 -0500

    v2.0

--- astsa_build/R/tsplot.R
+++ astsa_build/R/tsplot.R
@@ -50,14 +50,14 @@ tsplot <- function(x, y = NULL, main=NULL, ylab=NULL, xlab='Time', type=NULL,
    if(is.null(ylab)) {ylab = ifelse(is.null(y), deparse(substitute(x)), 
                       deparse(substitute(y)))}
    par(mar=c(2.5,2.5,1+topper,.5)+margins, mgp=c(1.6,.3,0)+mgpp, cex.main=1.2,  
-              tcl=-.2, cex.axis=.8, las=1)    
+              tcl=-.2, cex.axis=.8)    
    plot(x, y, type = type0, axes=FALSE, ann=FALSE, main=NULL, ... )
    brdr = par("usr")        
    rect(brdr[1], brdr[3], brdr[2], brdr[4], col=gray(.92), border='white')         
    Grid(minor=minor, nxm=nxm, nym=nym, xm.grid=xm.grid, ym.grid=ym.grid, col='white')
    par(new=TRUE)
    plot(x, y, type=type1, main=main, ylab=ylab, xlab=xlab, col=col, pch=pch, 
-         lty=lty, lwd=lwd, ... ) 
+         lty=lty, lwd=lwd, las=1, ... ) 
    box(col='white', lwd=2)
   } else {                 # multiple series
    if(!is.null(ylab)){ ylab = rep(ylab, ceiling(nser/length(ylab))) } 
