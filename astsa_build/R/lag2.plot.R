b4783e0093f0180cc16d2ede5bd7d7a54489b981
David Stoffer <stoffer@pitt.edu>
2022-12-21 16:13:12 -0500

    v2.0

--- astsa_build/R/lag2.plot.R
+++ astsa_build/R/lag2.plot.R
@@ -10,6 +10,7 @@ function(series1, series2, max.lag=0, corr=TRUE, smooth=TRUE, col=gray(.1),
    legend = graphics::legend
    #
    name1=paste(deparse(substitute(series1)),"(t-",sep="")
+   name10=paste(deparse(substitute(series1)),"(t)",sep="")
    name2=paste(deparse(substitute(series2)),"(t)",sep="")
    series1=as.ts(series1)
    series2=as.ts(series2)
@@ -21,8 +22,9 @@ function(series1, series2, max.lag=0, corr=TRUE, smooth=TRUE, col=gray(.1),
    old.par <- par(no.readonly = TRUE)
    par(mfrow=c(prow,pcol))
    for(h in 0:max.lag){                   
+    Xlab = ifelse(h==0, name10, paste(name1,h,")",sep="")) 
    tsplot(stats::lag(series1,-h), series2, xy.labels=FALSE, type='p', 
-               xlab=paste(name1,h,")",sep=""), ylab=name2, col=col, ...) 
+               xlab=Xlab, ylab=name2, col=col, ...) 
     if (smooth) 
     lines(stats::lowess(ts.intersect(stats::lag(series1,-h),series2)[,1],
                  ts.intersect(stats::lag(series1,-h),series2)[,2]), col=2, lwd=lwl)
