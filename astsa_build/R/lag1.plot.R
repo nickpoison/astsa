8333163a7f2e077dca4770f28b4e7a57b3b25d9b
David Stoffer <stoffer@pitt.edu>
2022-12-23 15:31:49 -0500

    v2.0

--- astsa_build/R/lag1.plot.R
+++ astsa_build/R/lag1.plot.R
@@ -1,32 +1,33 @@
 lag1.plot <-
 function(series, max.lag=1, corr=TRUE, smooth=TRUE, col=gray(.1), 
-         lwl=1, bgl=gray(1,.65), ltcol=1, box.col=8, cex=.9, ...){ 
-   #
-   as.ts = stats::as.ts
-   par = graphics::par
-   plot = graphics::plot
-   lines= graphics::lines
-   ts.intersect = stats::ts.intersect
-   legend = graphics::legend
-   #
-   name1=paste(deparse(substitute(series)),"(t-",sep="")
-   name2=paste(deparse(substitute(series)),"(t)",sep="")
-   series=as.ts(series)
-   max.lag=as.integer(max.lag)
-   prow=ceiling(sqrt(max.lag))
-   pcol=ceiling(max.lag/prow)
-   a=stats::acf(series,max.lag,plot=FALSE)$acf[-1]
-   old.par <- par(no.readonly = TRUE)
-   par(mfrow=c(prow,pcol))
-  for(h in 1:max.lag){                       
-   tsplot(stats::lag(series,-h), series, type='p', xy.labels=FALSE, 
-           xlab=paste(name1,h,")",sep=""), ylab=name2, col=col, cex=cex, ...) 
-    if (smooth) 
-    lines(stats::lowess(ts.intersect(stats::lag(series,-h),series)[,1],
-                 ts.intersect(stats::lag(series,-h),series)[,2]), col=2, lwd=lwl)
-    if (corr)
-    legend("topright", legend=round(a[h], digits=2), text.col=ltcol, bg=bgl, adj=.25, box.col=box.col)
-   on.exit(par(old.par))
-   }
+         lwl=1, lwc=2, bgl=gray(1,.65), ltcol=1, box.col=8, cex=.9, ...){ 
+  #
+  as.ts = stats::as.ts
+  par   = graphics::par
+  plot  = graphics::plot
+  lines = graphics::lines
+  ts.intersect = stats::ts.intersect
+  legend = graphics::legend
+  #
+  name1 = paste(deparse(substitute(series)),"(t-",sep="")
+  name2 = paste(deparse(substitute(series)),"(t)",sep="")
+  max.lag = as.integer(max.lag)
+  prow = ceiling(sqrt(max.lag))
+  pcol = ceiling(max.lag/prow)
+  a = stats::acf(series,max.lag,plot=FALSE)$acf[-1]
+  old.par <- par(no.readonly = TRUE)
+
+ par(mfrow=c(prow,pcol))
+ for(h in 1:max.lag){
+  tsplot(stats::lag(series,-h), series, type='p', xy.labels=FALSE, 
+          xlab=paste(name1,h,")",sep=""), ylab=name2, col=col, cex=cex, ...) 
+   if (smooth) 
+   lines(stats::lowess(ts.intersect(stats::lag(series,-h),series)[,1],
+                ts.intersect(stats::lag(series,-h),series)[,2]), col=lwc, lwd=lwl)
+   if (corr)
+   legend("topright", legend=round(a[h], digits=2), text.col=ltcol, bg=bgl, 
+           adj=.25, box.col=box.col, cex=.9)
+ }
+ on.exit(par(old.par))
 }
 
--- astsa_build/R/lag2.plot.R
+++ astsa_build/R/lag2.plot.R
@@ -1,37 +1,38 @@
 lag2.plot <-
 function(series1, series2, max.lag=0, corr=TRUE, smooth=TRUE, col=gray(.1),
-         lwl=1, bgl =gray(1,.65), ltcol=1, box.col=8, cex=.9, ...){ 
-   #
-   as.ts = stats::as.ts
-   par = graphics::par
-   plot = graphics::plot
-   lines= graphics::lines
-   ts.intersect = stats::ts.intersect
-   legend = graphics::legend
-   #
-   name1=paste(deparse(substitute(series1)),"(t-",sep="")
-   name10=paste(deparse(substitute(series1)),"(t)",sep="")
-   name2=paste(deparse(substitute(series2)),"(t)",sep="")
-   series1=as.ts(series1)
-   series2=as.ts(series2)
-   max.lag=as.integer(max.lag)
-   m1=max.lag+1
-   prow=ceiling(sqrt(m1))
-   pcol=ceiling(m1/prow)
-   a=stats::ccf(series1,series2,max.lag,plot=FALSE)$acf
-   old.par <- par(no.readonly = TRUE)
-   par(mfrow=c(prow,pcol))
-   for(h in 0:max.lag){                   
-    Xlab = ifelse(h==0, name10, paste(name1,h,")",sep="")) 
-   tsplot(stats::lag(series1,-h), series2, xy.labels=FALSE, type='p', 
-               xlab=Xlab, ylab=name2, col=col, cex=cex, ...) 
-    if (smooth) 
-    lines(stats::lowess(ts.intersect(stats::lag(series1,-h),series2)[,1],
-                 ts.intersect(stats::lag(series1,-h),series2)[,2]), col=2, lwd=lwl)
-    if (corr)
-    legend("topright", legend=round(a[m1-h], digits=2), text.col=ltcol, bg=bgl, adj=.25, box.col=box.col)             
-   on.exit(par(old.par))
-   }
+         lwl=1, lwc=2, bgl=gray(1,.65), ltcol=1, box.col=8, cex=.9, ...){ 
+#
+  as.ts = stats::as.ts
+  par   = graphics::par
+  plot  = graphics::plot
+  lines = graphics::lines
+  ts.intersect = stats::ts.intersect
+  legend = graphics::legend
+#
+  name1  = paste(deparse(substitute(series1)),"(t-",sep="")
+  name10 = paste(deparse(substitute(series1)),"(t)",sep="")
+  name2  =paste(deparse(substitute(series2)),"(t)",sep="")
+#
+  max.lag = as.integer(max.lag)
+  m1      = max.lag+1
+  prow    = ceiling(sqrt(m1))
+  pcol    = ceiling(m1/prow)
+  a       = stats::ccf(series1,series2,max.lag,plot=FALSE)$acf
+#
+ old.par <- par(no.readonly = TRUE)
+ par(mfrow = c(prow,pcol))
+ for(h in 0:max.lag){                   
+  Xlab = ifelse(h==0, name10, paste(name1,h,")",sep="")) 
+  tsplot(stats::lag(series1,-h), series2, xy.labels=FALSE, type='p', 
+           xlab=Xlab, ylab=name2, col=col, cex=cex, ...) 
+  if (smooth) 
+   lines(stats::lowess(ts.intersect(stats::lag(series1,-h),series2)[,1],
+                ts.intersect(stats::lag(series1,-h),series2)[,2]), col=lwc, lwd=lwl)
+  if (corr)
+   legend("topright", legend=round(a[m1-h], digits=2), text.col=ltcol, bg=bgl, 
+           adj=.25, box.col=box.col, cex=.9)
+ }
+ on.exit(par(old.par))
 }
 
 
