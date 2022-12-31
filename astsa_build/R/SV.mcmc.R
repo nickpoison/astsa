ac3253c7edc7d52fdf71b19bab41d0bd89fcf58a
David Stoffer <stoffer@pitt.edu>
2022-12-30 20:38:17 -0500

    v2.0

--- astsa_build/R/SV.mcmc.R
+++ astsa_build/R/SV.mcmc.R
@@ -151,7 +151,7 @@ par(mfcol=c(3,3))
 for (i in 1:3){
   tsplot(parms[,i], main=names[i], col=culer[i], ylab='', xlab='Index')
    ess = ESS(parms[,i])
-  legend("topright", legend=paste('ESS = ', round(ess, digits=1)), adj=.1, bg='white')	
+  legend("topright", legend=paste('ESS = ', round(ess, digits=1)), adj=.1, bg=gray(1,.8), box.col=8)
   acf1(parms[,i],   main='', col=culer[i], ylim=c(lwr,1))
   hist(parms[,i],   main='', xlab='', col=astsa.col(culer[i], .4))
   abline(v=c(stats::quantile(parms[,i], probs=c(.025,.5,.975))), col=8)
