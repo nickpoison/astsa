738a4587ff2d14ec6d9132c5e87168fe1103d3f7
David Stoffer <stoffer@pitt.edu>
2023-01-08 23:22:05 -0500

    v2.0

--- astsa_build/R/ARMAtoAR.R
+++ astsa_build/R/ARMAtoAR.R
@@ -1,6 +1,3 @@
 ARMAtoAR <- function(ar = 0, ma = 0, lag.max=20){
-             ARMAtoMA <- stats::ARMAtoMA
-             arinv <- -ma
-             mainv <- -ar
-             return(ARMAtoMA(arinv, mainv, lag.max))
+  return(stats::ARMAtoMA(ar=-ma, ma=-ar, lag.max))
 }
