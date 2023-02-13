746782162a8daf47344a1af9bde54713285c5964
David Stoffer <stoffer@pitt.edu>
2023-02-13 01:35:54 -0500

    v2.0+

--- astsa_build/R/stoch.reg.R
+++ astsa_build/R/stoch.reg.R
@@ -1,5 +1,5 @@
 stoch.reg <-
-function(xdata, cols.full, cols.red, alpha, L, M, plot.which, ...)  {
+function(xdata, cols.full, cols.red=NULL, alpha, L, M, plot.which, col.resp=NULL, ...)  {
 #
 ##############################################################################
 # This function computes the spectral matrix, F statistics and coherences, and plots them.
@@ -9,47 +9,50 @@ function(xdata, cols.full, cols.red, alpha, L, M, plot.which, ...)  {
 # columns of input series in the "full" and "reduced" regression models - enter NULL if there 
 # are no inputs under the reduced model. 
 #
-# The response variable is the LAST column of the data matrix, and this need not be specified 
-# among the inputs.  Other inputs are alpha (test size), L (smoothing), M (number 
-# of points in the discretization of the integral)  and plot.which = "coh" 
-# or "F", to plot either the coherences or the F statistics.
+# If the response variable is the LAST column of the data matrix, it need not be specified 
+# among the inputs.  Otherwise, specify the column as 'col.resp'. 
+#
+# Other inputs are alpha (test size), L (smoothing), M (number of points in the 
+# discretization of the integral)  and plot.which = "coh"  or "F", to plot either 
+# the coherences or the F statistics.
 ##############################################################################
 #
-# SPEC[i,j,k] is the spectrum between the i-th and j-th series at frequency k/n':
-SPEC = array(dim = c(ncol(xdata),ncol(xdata),nextn(nrow(xdata))/2)) 
-for(i in 1:ncol(xdata)) { 
-for (j in i:ncol(xdata)) {
-	power = stats::spec.pgram(xdata[,c(i,j)], kernel("daniell",(L-1)/2), plot = FALSE)
-	SPEC[i,i, ] = power$spec[,1]
-	SPEC[j,j, ] = power$spec[,2]
-	coh.ij = power$coh 
-	phase.ij = power$phase 
-	SPEC[i,j, ] = sqrt(coh.ij*power$spec[,1]*power$spec[,2])*exp(1i*phase.ij)
-	SPEC[j,i, ] = Conj(SPEC[i,j, ])
-	}}
+
+# checks
+L = floor(L)  # force L and M to be integers
+M = floor(M)
+if (L%%2 < 1) { L=L+1; cat('L is even and has been increased by 1 \n') }
+if (alpha < 0 || alpha > 1) stop('alpha must be in [0, 1]')
+#
+if (is.null(col.resp)) col.resp = ncol(xdata)  # the last column
+
+power = mvspec(xdata, kernel=kernel("daniell",(L-1)/2), plot = FALSE)
+SPEC  = power$fxx 
 
 ### Compute the power under the full model:
-f.yy = SPEC[ncol(xdata), ncol(xdata), ]
+f.yy = SPEC[col.resp,  col.resp,  ]
 f.xx = SPEC[cols.full, cols.full, ]
-f.xy = SPEC[cols.full, ncol(xdata), ]
-f.yx = SPEC[ncol(xdata), cols.full, ]
+f.xy = SPEC[cols.full, col.resp,  ]
+f.yx = SPEC[col.resp,  cols.full, ]
 
-power.full = vector(length = dim(SPEC)[3])
-for (k in 1:length(power.full)) {
-	power.full[k] = f.yy[k] - sum(f.yx[,k]*solve(f.xx[,,k],f.xy[,k]))
-	}
+power.full = c()
+ n.freq = power$n.used/2
+ for (k in 1:n.freq) {
+      power.full[k] = f.yy[k] - sum(f.yx[,k]*solve(f.xx[,,k],f.xy[,k]))
+ }
 power.full = Re(power.full)
 
 
 ### Compute the IFT of the coefficients in the full model:
-B = array(dim = c(length(cols.full), dim(SPEC)[3]))
+B = array(dim = c(length(cols.full), n.freq))
 for (k in 1:length(power.full)) {
-	B[,k] = solve(t(f.xx[,,k]),f.yx[,k])
-	}
+     B[,k] = solve(t(f.xx[,,k]), f.yx[,k])
+}
+
 # Isolate those frequencies at which we need B:
 # These are the frequencies 1/M, 2/M, ... .5*M/M 
 # Currently the frequencies used are 1/N, 2/N, ... .5*N/N 
-N = 2*length(power$freq)  # This will be n', in our notation 
+N = 2*n.freq  # This will be n', in our notation 
 # R displays the power at only half of the frequencies.
 sampled.indices = (N/M)*(1:(M/2))  # These are the indices of the frequencies we want
 B = B[, sampled.indices]
@@ -72,7 +75,7 @@ if (length(cols.red) > 0) {
 	f.yx = SPEC[ncol(xdata), cols.red, ]
 	}
 
-power.red = vector(length = dim(SPEC)[3])
+power.red = vector(length = n.freq)
 for (k in 1:length(power.red)) {
 	if(length(cols.red)==0) power.red[k] = f.yy[k]
 	if(length(cols.red)==1) power.red[k] = f.yy[k] - f.yx[k]*f.xy[k]/f.xx[k]
