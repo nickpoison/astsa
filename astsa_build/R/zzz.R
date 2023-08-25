8d1d17c558ac4345f6eb6a8632b3b44ac3e6bc6e
David Stoffer <stoffer@pitt.edu>
2023-08-24 20:57:21 -0400

    v2.0.1

--- astsa_build/R/zzz.R
+++ astsa_build/R/zzz.R
@@ -1,7 +1,8 @@
 .onLoad <- function(libname, pkgname){ 
 			 palette(c("black","#F6483C","#00BA38","#1874cd","#0D9AC0","#cd1874","#CD7118","gray62"))
+			 filter <- stats::filter
 			 }
 			 
 .onUnload <- function(libpath){ 
 			 palette("default")
-			 }	 
+			 }
