27f5f8fca98a33ab644628b51e2d04983d6ab0e4
Nicky Poison <nickpoison@hotmail.com>
2023-12-30 19:45:35 -0500

    v2.0.1.0020

--- astsa_build/R/sarima.R
+++ astsa_build/R/sarima.R
@@ -3,24 +3,23 @@ function(xdata,p,d,q,P=0,D=0,Q=0,S=-1,details=TRUE,xreg=NULL,Model=TRUE,
           fixed=NULL,tol=sqrt(.Machine$double.eps),no.constant=FALSE, data=NULL, ...)
 { 
 
-   if(!is.null(data)) { 
-      tp = tsp(as.ts(data))
-      suppressWarnings(rm(list = colnames(data),  envir = .GlobalEnv))
-      attach(as.data.frame(data), warn.conflicts = FALSE)  # detached later
-      xdata = ts(xdata, start = tp[1], frequency = tp[3])
-    } else {
-      xdata = as.ts(xdata)
-    }
-
+  if(!is.null(data)) {
+     tp = tsp(as.ts(data))
+     suppressWarnings(rm(list = colnames(data),  envir = .GlobalEnv))
+     attach(as.data.frame(data), warn.conflicts = FALSE)   # detached later
+     xdata = ts(xdata, start = tp[1], frequency = tp[3])
+   } else {
+     xdata = as.ts(xdata)
+  }
 
    trans = ifelse (is.null(fixed), TRUE, FALSE)
    trc   = ifelse(details, 1, 0)
    n     = length(xdata) 
- if (is.null(xreg)) {
+  if (is.null(xreg)) {
    constant = 1:n 
    xmean    = rep(1,n)  
    if (no.constant)  xmean=NULL 
-   if (d==0 & D==0) {	  
+   if (d==0 & D==0) {  
            fitit = arima(xdata, order=c(p,d,q), seasonal=list(order=c(P,D,Q), period=S),
                   xreg=xmean, include.mean=FALSE, fixed=fixed, transform.pars=trans, 
                   optim.control=list(trace=trc, REPORT=1, reltol=tol))
@@ -31,17 +30,18 @@ function(xdata,p,d,q,P=0,D=0,Q=0,S=-1,details=TRUE,xreg=NULL,Model=TRUE,
     } else fitit = arima(xdata, order=c(p,d,q), seasonal=list(order=c(P,D,Q), period=S), 
                    include.mean=!no.constant, fixed=fixed, transform.pars=trans, 
                    optim.control=list(trace=trc, REPORT=1, reltol=tol))
-  }
-#
+  } 
+
   if (!is.null(xreg)) {
     fitit = arima(xdata, order=c(p,d,q), seasonal=list(order=c(P,D,Q), period=S), 
                          xreg=xreg, fixed=fixed, transform.pars=trans, 
                          optim.control=list(trace=trc, REPORT=1, reltol=tol))
   }
-###########################
-   if (is.null(fixed)) {coefs=fitit$coef
+
+  if (is.null(fixed)) {coefs=fitit$coef
     } else { coefs = fitit$coef[is.na(fixed)]
-    }  
+    }
+  
    k       = length(coefs) 
    n       = fitit$nobs  # effective sample size
    dfree   = n-k 
@@ -52,16 +52,18 @@ function(xdata,p,d,q,P=0,D=0,Q=0,S=-1,details=TRUE,xreg=NULL,Model=TRUE,
    BIC     = stats::BIC(fitit)/n
    AIC     = stats::AIC(fitit)/n
    AICc    = (n*AIC + ( (2*k^2+2*k)/(n-k-1) ))/n
-  if(!is.null(data)) {detach(as.data.frame(data))}
+
+  if(!is.null(data)) {detach(as.data.frame(data))}   # detach data if there
+
 # print  results
-    cat('<><><><><><><><><><><><><><>')
-    cat('\n','\n')
-    cat('Coefficients:', '\n')
-    print(ttable)
-    cat('\n')
-    cat('sigma^2 estimated as', fitit$sigma2, 'on', dfree, 'degrees of freedom', '\n','\n')
-    cat('AIC =', AIC, ' AICc =', AICc, ' BIC =', BIC, '\n', '\n')
-   out = list(fit=fitit, degrees_of_freedom=dfree, ttable=ttable, 
+  cat('<><><><><><><><><><><><><><>')
+  cat('\n','\n')
+  cat('Coefficients:', '\n')
+  print(ttable)
+  cat('\n')
+  cat('sigma^2 estimated as', fitit$sigma2, 'on', dfree, 'degrees of freedom', '\n','\n')
+  cat('AIC =', AIC, ' AICc =', AICc, ' BIC =', BIC, '\n', '\n')
+  out = list(fit=fitit, degrees_of_freedom=dfree, ttable=ttable, 
              ICs=c(AIC=AIC, AICc=AICc, BIC=BIC))
 ############################
 
@@ -72,19 +74,21 @@ function(xdata,p,d,q,P=0,D=0,Q=0,S=-1,details=TRUE,xreg=NULL,Model=TRUE,
     rs <-  fitit$residuals 
    stdres <- rs/sqrt(fitit$sigma2)
    num    <- sum(!is.na(rs))
- tsplot(stdres, main = "Standardized Residuals", ylab = "", ...)
+  tsplot(stdres, main = "Standardized Residuals", ylab = "", ...)
     if(Model){
      if (S<0) {title(bquote('Model: ('~.(p)*','*.(d)*','*.(q)~')'), adj=0) }
-     else { title(bquote('Model: ('~.(p)*','*.(d)*','*.(q)~')'~'\u00D7'~'('~.(P)*','*.(D)*','*.(Q)~')'[~.(S)]), 
+     else {title(bquote('Model: ('~.(p)*','*.(d)*','*.(q)~')'~'\u00D7'~'('~.(P)*','*.(D)*','*.(Q)~')'[~.(S)]), 
                   adj=0) }     
     }
+
     alag  <- max(10+sqrt(num), 3*S) 
- acf1(rs, alag, main = "ACF of Residuals", ...)   
-#    
+  acf1(rs, alag, main = "ACF of Residuals", ...)
+
     u = qqnorm(stdres, plot.it=FALSE)
     lwr = min(-4, min(stdres, na.rm=TRUE)); upr = max(4, max(stdres), na.rm=TRUE)
- tsplot(u$x, u$y, type='p', ylim=c(lwr,upr), ylab="Sample Quantiles", xlab="Theoretical Quantiles",  
+  tsplot(u$x, u$y, type='p', ylim=c(lwr,upr), ylab="Sample Quantiles", xlab="Theoretical Quantiles",  
            main="Normal Q-Q Plot of Std Residuals",  ...)	
+
      ################ qq error bnds ###########
        sR  <- !is.na(stdres)
        ord <- order(stdres[sR])
@@ -112,11 +116,10 @@ function(xdata,p,d,q,P=0,D=0,Q=0,S=-1,details=TRUE,xreg=NULL,Model=TRUE,
     pval <- numeric(nlag)
     for (i in (ppq+1):nlag) {u <- stats::Box.test(rs, i, type = "Ljung-Box")$statistic
                              pval[i] <- stats::pchisq(u, i-ppq, lower.tail=FALSE)}            
- tsplot( (ppq+1):nlag, pval[(ppq+1):nlag], type='p', xlab = "LAG (H)", ylab = "p value", 
+  tsplot( (ppq+1):nlag, pval[(ppq+1):nlag], type='p', xlab = "LAG (H)", ylab = "p value", 
               ylim = c(-.14, 1), main = "p values for Ljung-Box statistic", ...)
-     abline(h = 0.05, lty = 2, col = 4)  
-    on.exit(par(old.par)) 
-
+   abline(h = 0.05, lty = 2, col = 4)  
+   on.exit(par(old.par)) 
 }  #  end new tsdiag
 invisible(out)
 }
--- astsa_build/R/sarima.for.R
+++ astsa_build/R/sarima.for.R
@@ -8,72 +8,76 @@ function(xdata,n.ahead,p,d,q,P=0,D=0,Q=0,S=-1,tol=sqrt(.Machine$double.eps),
   xname = deparse(substitute(xdata))
 
 
-     if(!is.null(data)){ 
-       tp = tsp(as.ts(data))
-       suppressWarnings(rm(list = colnames(data),  envir = .GlobalEnv))
-       attach(as.data.frame(data), warn.conflicts = FALSE)  # detached later
-       xdata = ts(xdata, start=tp[1], frequency=tp[3])
-      } else { xdata = as.ts(xdata)
-     }
+  if(!is.null(data)){ 
+    tp = tsp(as.ts(data))
+    suppressWarnings(rm(list = colnames(data),  envir = .GlobalEnv))
+    attach(as.data.frame(data), warn.conflicts = FALSE)  # detached later
+    xdata = ts(xdata, start=tp[1], frequency=tp[3])
+   } else { xdata = as.ts(xdata)
+  }
 
 
   n = length(xdata)
-if (is.null(xreg)) {  
-  constant=1:n
-  xmean = rep(1,n);  if(no.constant==TRUE) xmean=NULL
-  if (d==0 & D==0) {
-    fitit=stats::arima(xdata, order=c(p,d,q), seasonal=list(order=c(P,D,Q), period=S),
-            xreg=xmean,include.mean=FALSE,fixed=fixed,trans=trans,optim.control=list(reltol=tol));
-    nureg=matrix(1,n.ahead,1);  if(no.constant==TRUE) nureg=NULL          
-  } else if (xor(d==1, D==1) & no.constant==FALSE) {
-    fitit=stats::arima(xdata, order=c(p,d,q), seasonal=list(order=c(P,D,Q), period=S),
-            xreg=constant,fixed=fixed,trans=trans,optim.control=list(reltol=tol));
-    nureg=(n+1):(n+n.ahead)       
-  } else { fitit=stats::arima(xdata, order=c(p,d,q), seasonal=list(order=c(P,D,Q), period=S), 
-            fixed=fixed,trans=trans,optim.control=list(reltol=tol));
-          nureg=NULL   
+  if (is.null(xreg)) {  
+    constant=1:n
+    xmean = rep(1,n);  if(no.constant==TRUE) xmean=NULL
+    if (d==0 & D==0) {
+      fitit=stats::arima(xdata, order=c(p,d,q), seasonal=list(order=c(P,D,Q), period=S),
+              xreg=xmean,include.mean=FALSE,fixed=fixed,transform.pars=trans,optim.control=list(reltol=tol));
+      nureg=matrix(1,n.ahead,1);  if(no.constant==TRUE) nureg=NULL
+    } else if (xor(d==1, D==1) & no.constant==FALSE) {
+      fitit=stats::arima(xdata, order=c(p,d,q), seasonal=list(order=c(P,D,Q), period=S),
+              xreg=constant,fixed=fixed,transform.pars=trans,optim.control=list(reltol=tol));
+      nureg=(n+1):(n+n.ahead)       
+    } else { fitit=stats::arima(xdata, order=c(p,d,q), seasonal=list(order=c(P,D,Q), period=S), 
+              fixed=fixed,transform.pars=trans,optim.control=list(reltol=tol))
+      nureg=NULL   
+    }
   }
-}   
- if (!is.null(xreg)) {
-        fitit = stats::arima(xdata, order = c(p, d, q), seasonal = list(order = c(P, 
-            D, Q), period = S), xreg = xreg, fixed=fixed, trans=trans)
-        nureg = newxreg 
-}
-#--
-fore <- stats::predict(fitit, n.ahead, newxreg=nureg)  
-#-- graph:
-if (plot){
-  lines = graphics::lines
-  polygon = graphics::polygon
-  xy.coords = grDevices::xy.coords
-  time = stats::time
-  tsp  = stats::tsp
-  U  = fore$pred + 2*fore$se
-  L  = fore$pred - 2*fore$se
-  U1 = fore$pred + fore$se
-  L1 = fore$pred - fore$se
-  a  = ifelse(plot.all, 1, max(1,n-100))
-  minx = min(xdata[a:n],L)
-  maxx = max(xdata[a:n],U)
-   t1 = xy.coords(xdata, y = NULL)$x 
-   if(length(t1)<101) strt=t1[1] else strt=t1[length(t1)-100]
-   t2=xy.coords(fore$pred, y = NULL)$x 
-   endd=t2[length(t2)]
-   if(plot.all)  {strt=time(xdata)[1]} 
-   xllim=c(strt,endd)
-   typel = ifelse(plot.all, 'l', 'o')
-     if (!is.null(data)) { xdatanew= ts(c(xdata,fore$pred), start=tp[1], frequency=tp[3])
-    } else {
-    xdatanew = ts(c(xdata,fore$pred), start=tsp(xdata)[1], frequency=tsp(xdata)[3]) }
-  tsplot(xdatanew, type=typel, xlim=xllim, ylim=c(minx,maxx), ylab=xname, ...)    
-   xx = c(time(U), rev(time(U)))
-   yy = c(L, rev(U))
-   polygon(xx, yy, border=8, col=gray(.6, alpha=.2) ) 
-   yy1 = c(L1, rev(U1))
-   polygon(xx, yy1, border=8, col=gray(.6, alpha=.2) ) 
-   lines(fore$pred, col=2, type="o")
-}   
-if(!is.null(data)) {detach(as.data.frame(data))}
+  if (!is.null(xreg)) {
+      fitit = stats::arima(xdata, order = c(p, d, q), seasonal = list(order = c(P, 
+              D, Q), period = S), xreg = xreg, fixed=fixed, transform.pars=trans)
+      nureg = newxreg 
+  }
+
+if(!is.null(data)) {detach(as.data.frame(data))}   # detach data if there
+
+##--##
+fore <- stats::predict(fitit, n.ahead, newxreg=nureg)
+##--##
+
+##-- graph:
+  if (plot){
+    lines = graphics::lines
+    polygon = graphics::polygon
+    xy.coords = grDevices::xy.coords
+    time = stats::time
+    tsp  = stats::tsp
+    U  = fore$pred + 2*fore$se
+    L  = fore$pred - 2*fore$se
+    U1 = fore$pred + fore$se
+    L1 = fore$pred - fore$se
+    a  = ifelse(plot.all, 1, max(1,n-100))
+    minx = min(xdata[a:n],L)
+    maxx = max(xdata[a:n],U)
+     t1 = xy.coords(xdata, y = NULL)$x 
+     if(length(t1)<101) strt=t1[1] else strt=t1[length(t1)-100]
+     t2=xy.coords(fore$pred, y = NULL)$x 
+     endd=t2[length(t2)]
+     if(plot.all)  {strt=time(xdata)[1]} 
+     xllim=c(strt,endd)
+     typel = ifelse(plot.all, 'l', 'o')
+       if (!is.null(data)) { xdatanew= ts(c(xdata,fore$pred), start=tp[1], frequency=tp[3])
+      } else {
+      xdatanew = ts(c(xdata,fore$pred), start=tsp(xdata)[1], frequency=tsp(xdata)[3]) }
+    tsplot(xdatanew, type=typel, xlim=xllim, ylim=c(minx,maxx), ylab=xname, ...)    
+      xx = c(time(U), rev(time(U)))
+      yy = c(L, rev(U))
+     polygon(xx, yy, border=8, col=gray(.6, alpha=.2) ) 
+      yy1 = c(L1, rev(U1))
+     polygon(xx, yy1, border=8, col=gray(.6, alpha=.2) ) 
+     lines(fore$pred, col=2, type="o")
+  } 
   return(fore)
 }
 
