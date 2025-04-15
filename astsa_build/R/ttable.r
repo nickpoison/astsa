c5e84c3dc5f30bb847da62ebcb1969c8faeb19de
Nicky Poison <stoffer@pitt.edu>
2025-04-15 16:40:08 -0400

    v2.2+

--- astsa_build/R/ttable.r
+++ astsa_build/R/ttable.r
@@ -38,24 +38,28 @@ function (obj, digits = 4, vif = FALSE, ...){
         .stopquiet()
         }
        }
+# 
+   if (vif){
+ # check if factors
        if (!is.null(obj$contrasts)){ ucfactors = TRUE; vif = FALSE}
-       # check if dummy variables used ##########
+ # check if dummy variables used 
       if (attr(x$coef, 'dimnames')[[1]][1] ==  "(Intercept)"){
        tempx = as.matrix(model.matrix(obj)[,-1]) 
       } else { tempx = as.matrix(model.matrix(obj)) }
        for (i in 1:ncol(tempx)){
-       if ( sum( tempx[,i] %in% c(0,1)) > 1){
+       if ( sum( tempx[,i] %in% c(-1,0,1)) > 1){
            ucfactors = TRUE; vif = FALSE} 
        } 
-#      # check if only one ind variable
-#      if (vif){
-#        terms <- labels(coef(obj))
-#        n.terms <- length(terms)
-#       if (n.terms < 2) { 
-#         vif = FALSE
-#         cat("No VIFs printed because model contains fewer than 2 terms. \n")
-#        }
-#      }
+ # check if only one ind variable
+        terms <- labels(coef(obj))
+        n.terms <- length(terms) - ("(Intercept)" %in% terms)
+       if (n.terms < 2) { 
+         vif = FALSE
+         cat("No VIFs printed because model contains fewer than 2 terms. \n")
+        }
+   if (ucfactors) cat('Note:  No VIFs are printed because the model includes factors.\n')
+   }
+
 ##################
        if (!vif) {
         cat("\nCoefficients:\n")
@@ -91,7 +95,7 @@ function (obj, digits = 4, vif = FALSE, ...){
             coefs[!aliased, ] <- cbind(x$coefficients, VIF)
         }
         }
-        print(round(coefs,digits), na.print = " ", ... )  
+        print(round(coefs, digits), na.print = " ", ... )  
 }
     k = nrow(coefs)
     aicc = ( num*aic + ((2*k^2+2*k)/(num-k-1)) )/num
@@ -110,9 +114,9 @@ function (obj, digits = 4, vif = FALSE, ...){
                            x$fstatistic[3L], lower.tail = FALSE),
                         digits = digits))
         cat("\n")
-        cat('AIC = ',  round(aic,  digits+1), '  ',
-            'AICc = ', round(aicc, digits+1), '  ', 
-            'BIC = ',  round(bic,  digits+1), '\n') 
+        cat('AIC = ',  round(aic,  digits), '  ',
+            'AICc = ', round(aicc, digits), '  ', 
+            'BIC = ',  round(bic,  digits), '\n') 
     }
     correl <- x$correlation
     if (!is.null(correl)) {
@@ -128,8 +132,7 @@ function (obj, digits = 4, vif = FALSE, ...){
             }
 	}
     }
-    if (ucfactors) cat('\nNote:','No VIFs are printed because the model includes factors.')
-    cat("\n")#- not in S
+    cat("\n")
     invisible(x)
 }
 
@@ -158,24 +161,13 @@ function (obj, digits = 4, vif = FALSE, ...){
   terms <- colnames(modmat)
   n.terms <- ncol(modmat)
 } 
-#
-  if (n.terms < 2) {
-     cat("No VIFs printed because model contains fewer than 2 terms. \n")
-     return(NA); .stopquiet()
-  }
   R <- cov2cor(v)
   detR <- det(R)
-  result <- c()#matrix(0, n.terms, 3)
-#  rownames(result) <- terms
- # colnames(result) <- c("GVIF", "Df", "GVIF^(1/(2*Df))")
+  result <- c()
   for (term in 1:n.terms) {
     subs <- which(assign == term)
     result[term]  <- det(as.matrix(R[subs, subs])) * det(as.matrix(R[-subs, -subs]))/detR
-    #result[term, 2] <- length(subs)
   }
-#   if (all(result[, 2] == 1)) 
-#    result <- result[, 1]
-#    else result[, 3] <- result[, 1]^(1/(2 * result[, 2]))
   return(result)
 }
 
@@ -185,37 +177,3 @@ function (obj, digits = 4, vif = FALSE, ...){
   on.exit(options(opt))
   stop()
 }
-
-
-# remove me
-#.VIF_orig <- function (mod) 
-#{
-#  if (any(is.na(coef(mod)))) 
-#    stop("there are aliased coefficients in the model")
-#  v <- vcov(mod)
-#  assign <- attr(model.matrix(mod), "assign")
-#  if (names(coefficients(mod)[1]) == "(Intercept)") {
-#    v <- v[-1, -1]
-#    assign <- assign[-1]
-#  }
-#  else warning("No intercept: vifs may not be sensible.")
-#  terms <- labels(terms(mod))
-#  n.terms <- length(terms)
-#  if (n.terms < 2) 
-#    stop("model contains fewer than 2 terms")
-#  R <- cov2cor(v)
-#  detR <- det(R)
-#  result <- matrix(0, n.terms, 3)
-#  rownames(result) <- terms
-#  colnames(result) <- c("GVIF", "Df", "GVIF^(1/(2*Df))")
-#  for (term in 1:n.terms) {
-#    subs <- which(assign == term)
-#    result[term, 1] <- det(as.matrix(R[subs, subs])) * det(as.matrix(R[-subs, 
-#                                                                       -subs]))/detR
-#    result[term, 2] <- length(subs)
-#  }
-#  if (all(result[, 2] == 1)) 
-#    result <- result[, 1]
-#  else result[, 3] <- result[, 1]^(1/(2 * result[, 2]))
-#  result
-#}
