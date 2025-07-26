def5088cc6394a3c215b142df82bbe67c8448834
Nicky Poison <nickpoison@hotmail.com>
2025-07-26 15:51:31 -0400

    v2.3

--- astsa_build/R/astsa.col.r
+++ astsa_build/R/astsa.col.r
@@ -1,4 +1,5 @@
-astsa.col <- function(col=1, alpha=1, wheel=FALSE, pie=FALSE, num) {  
+astsa.col <- 
+function(col=1, alpha=1, wheel=FALSE, pie=FALSE, num, sat=NULL, val=NULL){  
   if (!wheel){
    u <- c("black","#F6483C","#00BA38","#1874cd","#0D9AC0","#cd1874","#CD7118","gray62")
    culers = u[(col+7)%%8 + 1]
@@ -10,11 +11,12 @@ astsa.col <- function(col=1, alpha=1, wheel=FALSE, pie=FALSE, num) {
    num = as.integer(num)
    hsv = rgb2hsv(col2rgb(col))
     h   = hsv[1]
-    s   = hsv[2]
-    v   = hsv[3]
+    s   = ifelse(is.null(sat), hsv[2], sat %% 1)
+    v   = ifelse(is.null(val), hsv[3], val %% 1)
    hues = seq(h, h + 1, by=1/num)[1:num] %% 1
    culers =  hsv(hues, s, v, alpha=alpha)
    if (pie){ pie(rep(1,num), col=culers) }
    return(culers)
   }
 }
+
