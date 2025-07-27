8d6913fbba369c0c2c1abe5a6f2a878b81130494
Nicky Poison <nickpoison@hotmail.com>
2025-07-27 01:16:00 -0400

    v2.3

--- astsa_build/R/astsa.col.r
+++ astsa_build/R/astsa.col.r
@@ -4,19 +4,17 @@ function(col=1, alpha=1, wheel=FALSE, pie=FALSE, num, sat=NULL, val=NULL){
    u <- c("black","#F6483C","#00BA38","#1874cd","#0D9AC0","#cd1874","#CD7118","gray62")
    culers = u[(col+7)%%8 + 1]
    culers = adjustcolor(culers, alpha.f=alpha)
-   if (pie){ pie(rep(1,length(col)), col=culers) }
-   invisible(culers)
   } else {  # make wheel
    if (missing(num)) num = readline(prompt="How many colors do you want? ")
    num = as.integer(num)
    hsv = rgb2hsv(col2rgb(col))
     h   = hsv[1]
-    s   = ifelse(is.null(sat), hsv[2], sat %% 1)
-    v   = ifelse(is.null(val), hsv[3], val %% 1)
+    s   = ifelse(is.null(sat), hsv[2], sat)
+    v   = ifelse(is.null(val), hsv[3], val)
    hues = seq(h, h + 1, by=1/num)[1:num] %% 1
    culers =  hsv(hues, s, v, alpha=alpha)
-   if (pie){ pie(rep(1,num), col=culers) }
-   invisible(culers)
   }
+   if (pie){ pie(rep(1,length(culers)), col=culers) }
+   invisible(culers)
 }
 
